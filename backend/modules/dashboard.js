ea40a4f8a6156668776d14fb852be7f351e0c56c
Apple <apple@Apples-MacBook-Air.local>
2024-11-15 20:41:04 +0530

    attendance post request

--- backend/modules/dashboard.js
+++ backend/modules/dashboard.js
@@ -1,6 +1,11 @@
 const express = require('express')
 const router = express.Router()
 const db = require('./db')
+const joi = require('joi')
+const schema = joi.object({
+    employee_id: joi.number().integer().required(),
+    status: joi.string().valid('Present','Absent','Holiday','On Leave').required()
+})
 router.use(express.json());
 router.get('/attendance/:id/:month', async (req, res) => {
     try {
@@ -21,25 +26,44 @@ router.get('/attendance/:id/:month', async (req, res) => {
             onleave: od[0]["COUNT(date)"]
         });
     }
-catch(err){
-    console.log(err);
-    res.status(501).json({
-        msg:"error in fetching data from the database"
-    })
-}       
+    catch (err) {
+        console.log(err);
+        res.status(501).json({
+            msg: "error in fetching data from the database"
+        })
+    }
 })
-// router.post('/attendance',async (req,res)=>
-// {
-//     const values = [req.attendance_id,req.employee-id,req.date,req.status]
-//     const query = 'INSERT INTO ATTENDANCE(employee_id,date,status) VALUES(?,?,?,?)'
-//     await db.query(query,values,(err,results)=>
-//     {
-//         if(err)
-//             return callback(err);
-//         else
-//             callback(null,results);
-//     })
-// })
+router.post('/attendance', async (req, res) => {
+    try {
+        const {err} = schema.validate(req.body)
+        if(err)
+        {
+            res.status(500).json(
+                {
+                    msg:"validation error"
+                }
+            );
+        }
+        const today = new Date();
+        const year = String(today.getFullYear());
+        const month = String(today.getMonth() + 1).padStart(2, '0'); // JavaScript months are 0-indexed
+        const day = String(today.getDate()).padStart(2, '0');
+
+        const formattedDate = `${year}-${month}-${day}`;
+        const values = [req.body.employee_id, formattedDate, req.body.status]
+        const query = 'INSERT INTO Attendance(employee_id,date,status) VALUES(?,?,?)'
+        await db.promise().query(query, values);
+        res.status(201).json({
+            msg:"successfully added attendance to the database"
+        });
+    }
+    catch (err) {
+        console.log(err);
+        res.status(501).json({
+            msg: "error in updation from the database"
+        })
+    }
+});
 // // router.get('/salary',async (req,res)=>
 // // {
 // //     const valuesp = [req.employee_id,'Present']
