dbeb74b9d5c4d8fec668b6d3ea9ade2e1c3c8dbc
Sanjan-rao <usanjanrao@gmail.com>
2024-12-31 13:45:33 +0530

    fixing frontend url

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0', // or a specific IP like '192.168.1.100'
+    host: true, // or a specific IP like '192.168.1.100'
     port: 3000,      // Set your desired port (optional)
   },
 })
