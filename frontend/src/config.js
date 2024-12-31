6ce922a247649efffa33f8f7ad93591d019c3e23
Sanjan-rao <usanjanrao@gmail.com>
2024-12-31 12:35:16 +0530

    Made hosting changes

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,6 +1,6 @@
 // src/config.js
 const CONFIG = {
-    BACKEND_URL: "http://localhost:10423", // Replace 10423 with your desired port
+    BACKEND_URL: "https://threadsandco-erp-backend.onrender.com", // Replace 10423 with your desired port
   };
   
   export default CONFIG;
