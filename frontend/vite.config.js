c3d7b8d083c5368d3a7b7eb37ae9605a1cd96494
Sanjan-rao <usanjanrao@gmail.com>
2024-12-31 13:05:32 +0530

    fixing frontend host address issue

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // or a specific IP like '192.168.1.100'
+    port: 5173,      // Set your desired port (optional)
+  },
 })
