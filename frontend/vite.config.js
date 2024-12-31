a1b3cc0129a1756378f292aa5eb70c0b73ab1840
Sanjan-rao <usanjanrao@gmail.com>
2024-12-31 13:30:52 +0530

    changed frontend port

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,6 +6,6 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0', // or a specific IP like '192.168.1.100'
-    port: 5173,      // Set your desired port (optional)
+    port: 3000,      // Set your desired port (optional)
   },
 })
